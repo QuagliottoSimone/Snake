1a0aa348502134f4233ce78aaf91c3af6fb7ac35
quagliottosimone <quagliottosimone@Lab06_28.itseinaudi.local>
2016-02-13 10:40:09 +0100

    Snake

---
+++ Snake/src/Punto.java
@@ -0,0 +1,59 @@
+
+public class Punto {
+	private int x;
+	private int y;
+
+	public Punto() {
+		x = 3;
+		y = 3;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		if (x >= 0  && x <= 200) {
+			this.x = x;
+		}
+
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		if (y >= 0 && y <= 100) {
+			this.y = y;
+		}
+	}
+
+	public Punto(int z, int k) {
+		setX(z);
+		setY(k);
+
+	}
+
+	public Punto(Punto q) {
+		setX(q.getX());
+		setY(q.getY());
+	}
+
+	public String toString() {
+		return "Punto x=" + x + ", y=" + y+" ";
+	}
+
+	public boolean equals(Punto q) {
+		if (this.x == q.getX() && this.y == q.getY()) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+	public Punto clone(){
+		return new Punto(this);
+	}
+
+}
--- Snake/src/Snake.java
+++ Snake/src/Snake.java
@@ -4,10 +4,13 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.widgets.Canvas;
 
 public class Snake {
 
 	protected Shell shell;
+	private Punto testa = new Punto ()
 
 	/**
 	 * Launch the application.
@@ -42,8 +45,12 @@ public class Snake {
 	 */
 	protected void createContents() {
 		shell = new Shell();
-		shell.setSize(450, 300);
+		shell.setSize(509, 311);
 		shell.setText("SWT Application");
+		
+		Canvas canvas = new Canvas(shell, SWT.NONE);
+		canvas.setBounds(10, 10, 350, 250);
+		GC gc = new GC(canvas);
 		
 		Button btnProva = new Button(shell, SWT.NONE);
 		btnProva.addSelectionListener(new SelectionAdapter() {
@@ -52,8 +59,26 @@ public class Snake {
 				System.out.println("Okay");
 			}
 		});
-		btnProva.setBounds(349, 10, 75, 25);
-		btnProva.setText("Prova");
+		btnProva.setBounds(382, 10, 102, 25);
+		btnProva.setText("Inizia");
+		
+		Button btnV = new Button(shell, SWT.NONE);
+		btnV.setBounds(418, 72, 30, 25);
+		btnV.setText("v");
+		
+		Button button = new Button(shell, SWT.NONE);
+		button.setText("^");
+		button.setBounds(419, 41, 30, 25);
+		
+		Button button_1 = new Button(shell, SWT.NONE);
+		button_1.setText("<");
+		button_1.setBounds(382, 72, 30, 25);
+		
+		Button button_2 = new Button(shell, SWT.NONE);
+		button_2.setText(">");
+		button_2.setBounds(454, 72, 30, 25);
+		
+		
 
 	}
 }
